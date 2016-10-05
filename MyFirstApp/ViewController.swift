50ffde91566b92cd2ec078eb416ba9bc00a0a9f1
YOUICHI <youichi@YOUICHI-no-Mac-mini.local>
2016-10-06 04:33:14 +0900

    first commit

---
+++ MyFirstApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MyFirstApp
+//
+//  Created by YOUICHI on 2016/10/06.
+//  Copyright © 2016 YOUICHI. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
